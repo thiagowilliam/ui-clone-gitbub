742005078e0789262bfb4f65e43f51f040601438
Thiago William <thiago.will.silva@gmail.com>
2020-08-18 20:07:04 -0300

    header navigate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import * as S from './styles';
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate(`/${search.toLowerCase().trim()}`);
+  }
+
   return (
     <S.Container>
       <S.GithubLogo />
-      <S.SearchForm>
-        <input type="text" placeholder="Enter Userbane or Repo" />
+      <S.SearchForm onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Userbane or Repo..."
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
       </S.SearchForm>
     </S.Container>
   );
