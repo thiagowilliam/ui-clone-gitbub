d5e98940c59e8972f70e343a6a58cbbc2398ffc8
Thiago William <thiago.will.silva@gmail.com>
2020-08-18 20:15:58 -0300

    created footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import 'react-calendar-heatmap/dist/styles.css';
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Profile from './pages/Profile';
 import Repo from './pages/Repo';
+
 import GlobalStyles from './styles/GlobalStyles';
 
 const App: React.FC = () => {
@@ -19,7 +22,7 @@ const App: React.FC = () => {
         <Route path="/:username/:reponame" element={<Repo />} />
       </Routes>
 
-      {/* <Footer /> */}
+      <Footer />
       <GlobalStyles />
     </BrowserRouter>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Line />
+      <S.GithubLogo />
+    </S.Container>
+  );
+};
+
+export default Footer;
